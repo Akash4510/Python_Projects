473dcba2606de73f40ddd560ba20802d1a1f2136
Akash Gupta <78362903+Akash4510@users.noreply.github.com>
2021-05-22 00:42:17 +0530

    Added Single player mode (with computer).
    This is a Tic Tac Toe game with both single-player and multiplayer modes.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,180 +1,317 @@
-def display_board(board):
-    print('  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
-    print('-----------------')
-    print('  ' + board[4] + '  |  ' + board[5] + '  |  ' + board[6])
-    print('-----------------')
-    print('  ' + board[7] + '  |  ' + board[8] + '  |  ' + board[9])
+import random
 
 
-def player_choice():
-    x = False
+class Player:
 
-    while not x:
-        p_choice = input("What do you want to select 'X' or 'O': ")
+    def __init__(self, symbol):
 
-        if p_choice.upper() == 'X':
-            p_player = 'Player1'
-            x = True
+        self.symbol = symbol
 
-        elif p_choice.upper() == 'O':
-            p_player = 'Player2'
-            x = True
+    def __str__(self):
+
+        return f"Player with symbol {self.symbol}"
+
+    def play(self, board):
+
+        while True:
+
+            player_position = input("Select a position [1-9]: ")
+
+            if player_position.isdigit():
+                if int(player_position) in range(1, 10):
+                    if board[int(player_position)] == ' ':
+                        board[int(player_position)] = self.symbol
+                        break
+                    elif board[int(player_position)] != ' ':
+                        print("Sorry! that position is already filled up.")
+                else:
+                    print("Select a value between 1 & 9 only!")
+                    continue
+            else:
+                print("Please select a digit [1-9]")
+                continue
+
+
+class Computer:
+
+    def __init__(self, symbol):
+
+        self.symbol = symbol
+
+    def __str__(self):
+
+        return f"Computer player with symbol {self.symbol}"
+
+    def play(self, board, pl_symbol):
+
+        available_positions = [x for x, letter in enumerate(board) if letter == ' ' and x != 0]
+        corner_positions = [1, 3, 7, 9]
+        edge_positions = [2, 4, 8, 6]
+        middle_position = 5
+
+        for i in range(1, 10):
+            if board[i] == ' ':
+                testing_board = board.copy()
+                testing_board[i] = self.symbol
+                if win_check(testing_board, self.symbol):
+                    board[i] = self.symbol
+                    return i
+
+        for s in range(1, 10):
+            if board[s] == ' ':
+                testing_board = board.copy()
+                testing_board[s] = pl_symbol
+                if win_check(testing_board, pl_symbol):
+                    board[s] = self.symbol
+                    return s
+
+        corners_available = []
+        for i in available_positions:
+            if i in corner_positions:
+                corners_available.append(i)
+        if len(corners_available) > 0:
+            move = random.choice(corners_available)
+            board[move] = self.symbol
+            return move
+
+        if board[middle_position] == ' ':
+            board[middle_position] = self.symbol
+            return middle_position
+
+        edges_available = []
+        for v in available_positions:
+            if v in edge_positions:
+                edges_available.append(v)
+        if len(edges_available) > 0:
+            r_c = random.choice(edges_available)
+            board[r_c] = self.symbol
+            return r_c
+
+
+def display_game(board):
+    print(f"  {board[7]}  |  {board[8]}  |  {board[9]}  ")
+    print("-----------------")
+    print(f"  {board[4]}  |  {board[5]}  |  {board[6]}  ")
+    print("-----------------")
+    print(f"  {board[1]}  |  {board[2]}  |  {board[3]}  ")
+
+
+def mode_selection():
+    while True:
+
+        mode = input(
+            "Which mode do you want to play?\nPress '1' for single player mode  |   Press '2' for two players mode: ")
+
+        if mode.isdigit():
+
+            if int(mode) == 1:
+                print("\nSINGLE PLAYER MODE\n")
+                return 1
+            elif int(mode) == 2:
+                print("\nTWO PLAYERS MODE\n")
+                return 2
+            else:
+                print("Please enter a valid input.")
+                continue
 
         else:
-            print("Sorry! I didn't understood.")
-            print("Please select between 'X' or 'O' only.")
-            x = False
-
-    print('\n')
-    print("You are", p_player)
-    print('\n')
-    print("Player1 = 'X'\nPlayer2 = 'O'")
+            print("Please enter a valid input.")
+            continue
 
 
-def r_position(my_list):
-    cp = False
+def symbol_selection():
+    while True:
+        s_choice = input("What do you want to be?\n'X' or 'O': ")
+        print("\n")
 
-    while not cp:
-        pos = input("Select a position: ")
-
-        if pos.isdigit() and int(pos) in range(1, 10):
-            if my_list[int(pos)] == ' ':
-                cp = True
-
-            elif my_list[int(pos)] != ' ':
-                print("Sorry! that position is already filled up.")
-                cp = False
-
-        if pos.isdigit() and int(pos) not in range(1, 10):
-            print("Please enter a valid position (1-9).")
-            cp = False
-
-        if not pos.isdigit():
-            print("Sorry!", pos, "is not a digit.")
-            print("Please enter a digit (1 - 9).")
-            cp = False
-
-    return int(pos)
+        if s_choice.lower() == 'x':
+            print("You selected 'X'\nYou (X) will go first. Computer (O) will go second.")
+            return 'X'
+        elif s_choice.lower() == 'o':
+            print("You selected 'O'\nComputer (X) will go first. You (O) will go second.")
+            return 'O'
+        else:
+            print("Sorry I didn't understood.\nPlease select 'X' or 'O'")
+            continue
 
 
-def replace(list_board, i_position, obj):
-    list_board[i_position] = obj
+def win_check(board, symbol):
 
-    return list_board
-
-
-def win_check(board, mark1):
-    if board[1] == board[2] == board[3] == mark1:
-        p_win = True
-    elif board[4] == board[5] == board[6] == mark1:
-        p_win = True
-    elif board[7] == board[8] == board[9] == mark1:
-        p_win = True
-    elif board[1] == board[4] == board[7] == mark1:
-        p_win = True
-    elif board[2] == board[5] == board[8] == mark1:
-        p_win = True
-    elif board[3] == board[6] == board[9] == mark1:
-        p_win = True
-    elif board[1] == board[5] == board[9] == mark1:
-        p_win = True
-    elif board[3] == board[5] == board[7] == mark1:
-        p_win = True
+    if board[1] == board[2] == board[3] == symbol:
+        win = True
+    elif board[4] == board[5] == board[6] == symbol:
+        win = True
+    elif board[7] == board[8] == board[9] == symbol:
+        win = True
+    elif board[1] == board[4] == board[7] == symbol:
+        win = True
+    elif board[2] == board[5] == board[8] == symbol:
+        win = True
+    elif board[3] == board[6] == board[9] == symbol:
+        win = True
+    elif board[1] == board[5] == board[9] == symbol:
+        win = True
+    elif board[3] == board[5] == board[7] == symbol:
+        win = True
     else:
-        p_win = False
+        win = False
 
-    return p_win
+    return win
 
 
-def draw_check(t_list):
-    for i in range(0, 10):
-        if t_list[i] == ' ':
+def draw_check(board):
+    for i in range(1, 10):
+        if board[i] == ' ':
             return False
-    return True
+    else:
+        return True
 
 
 def replay():
     while True:
-        choice = input("Would you like to keep playing? Y or N: ")
-
-        if choice.lower() == 'y':
-            game_on_1 = True
-            break
-        elif choice.lower() == 'n':
-            game_on_1 = False
-            break
+        ch = input("Do you want to play again?\n'Y' or 'N': ")
+        if ch.lower() == 'y' or ch.lower() == 'yes':
+            return True
+        elif ch.lower() == 'n' or ch.lower() == 'no':
+            return False
         else:
-            print("Sorry! I didn't understood, please select 'y' for 'yes' and 'n' for 'no'.")
+            print("Sorry! I didn't understood, please select 'Y' for 'Yes' and 'N' for 'No'.")
             continue
 
-    return game_on_1
 
+def single_player_game():
 
-board_list = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    player_symbol = symbol_selection()
+    print("\n")
+    if player_symbol == 'X':
+        computer_symbol = 'O'
+    else:
+        computer_symbol = 'X'
 
+    real_player = Player(player_symbol)
+    comp_player = Computer(computer_symbol)
 
-while True:
-    print('\n')
-    print("WELCOME TO TIC TAC TOE !")
-    print('\n')
-    player_choice()
-    print('\n')
-    input("Press ENTER to continue")
-    print('\n')
-    print("To choose a position, please refer the table below: ")
-    print('\n')
-    ins_list = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    display_board(ins_list)
-    print('\n')
-    input("Press ENTER to continue")
-    print('\n')
-    board_list = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    display_board(board_list)
+    display_game(board_list)
+    print("\n")
 
     game_on = True
-
     while game_on:
 
+        if player_symbol == 'X':
+
+            print(f"Your Turn ({player_symbol})")
+            real_player.play(board_list)
+            print('\n')
+            display_game(board_list)
+            print('\n')
+            if win_check(board_list, player_symbol):
+                print("HURRAY! YOU WON THE GAME :)\n")
+                break
+            elif draw_check(board_list):
+                print("IT'S A TIE!\n")
+                break
+
+            print(f"Computer's Turn ({computer_symbol})")
+            comp_player.play(board_list, player_symbol)
+            print('\n')
+            display_game(board_list)
+            print('\n')
+            if win_check(board_list, computer_symbol):
+                print("OOPS! YOU LOST THE GAME :(\n")
+                break
+            elif draw_check(board_list):
+                print("IT'S A TIE!\n")
+                break
+
+        elif player_symbol == 'O':
+
+            print(f"Computer's Turn ({computer_symbol})")
+            comp_player.play(board_list, player_symbol)
+            print('\n')
+            display_game(board_list)
+            print('\n')
+            if win_check(board_list, computer_symbol):
+                print("OOPS! YOU LOST THE GAME :(\n")
+                break
+            elif draw_check(board_list):
+                print("IT'S A TIE!\n")
+                break
+
+            print(f"Your Turn ({player_symbol})")
+            real_player.play(board_list)
+            print('\n')
+            display_game(board_list)
+            print('\n')
+            if win_check(board_list, player_symbol):
+                print("HURRAY! YOU WON THE GAME :)\n")
+                break
+            elif draw_check(board_list):
+                print("IT'S A TIE!\n")
+                break
+
+
+def two_players_game():
+
+    player_1 = Player('X')
+    player_2 = Player('O')
+    print("Player One (X) will go first.")
+    print("\n")
+    display_game(board_list)
+    print("\n")
+
+    game_on = True
+    while game_on:
+
+        print("Player One's turn (X)")
+        player_1.play(board_list)
         print('\n')
-        print("Player1 (X)'s turn")
-        mark = 'X'
-        position = r_position(board_list)
-        board_list = replace(board_list, position, mark)
+        display_game(board_list)
         print('\n')
-        display_board(board_list)
-        win = win_check(board_list, mark)
-        draw = draw_check(board_list)
-        if win:
-            print('\n')
-            print("Congratulations! Player1(X) wins the game.")
-            print('\n')
+        if win_check(board_list, 'X'):
+            print("HURRAY! Player One (X) WON THE GAME :)\n")
             break
-        if draw:
-            print('\n')
-            print("It's a DRAW")
-            print('\n')
+        elif draw_check(board_list):
+            print("IT'S A TIE!\n")
             break
-        
+
+        print("Player Two's turn (O)")
+        player_2.play(board_list)
+        print('\n')
+        display_game(board_list)
+        print('\n')
+        if win_check(board_list, 'O'):
+            print("HURRAY! Player Two (O) WON THE GAME :)\n")
+            break
+        elif draw_check(board_list):
+            print("IT'S A TIE!\n")
+            break
+
+
+if __name__ == '__main__':
+
+    while True:
 
         print('\n')
-        print("Player2 (O)'s turn")
-        mark = 'O'
-        position = r_position(board_list)
-        board_list = replace(board_list, position, mark)
+        print("WELCOME TO TIC TAC TOE !")
         print('\n')
-        display_board(board_list)
-        win = win_check(board_list, mark)
-        draw = draw_check(board_list)
-        if win:
-            print('\n')
-            print("Congratulations! Player2(O) wins the game.")
-            print('\n')
+
+        desired_mode = mode_selection()
+
+        print("To choose a position, please refer the table below: ")
+        print('\n')
+        ins_list = ['#', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        display_game(ins_list)
+        print('\n')
+        input("Press enter to continue")
+        print('\n')
+        board_list = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+        if desired_mode == 1:
+            single_player_game()
+
+        else:
+            two_players_game()
+
+        if not replay():
+            print("\nTHANKS FOR PLAYING !\n")
             break
-        if draw:
-            print('\n')
-            print("It's a DRAW")
-            print('\n')
-            break
-        
-    if not replay():
-        break
